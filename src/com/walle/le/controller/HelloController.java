991bef8646022c342811a5e628df1f29ba5eb25a
walle <lhphuong86@gmail.com>
2015-12-14 11:40:57 +0700

    this is walle

---
+++ src/com/walle/le/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.walle.le.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.walle.le.common.constant.ResponseType;
+import com.walle.le.common.exception.WalleCoreException;
+import com.walle.le.json.JsonResponse;
+import com.walle.le.vo.User;
+
+
+/**
+ * 
+ * @author walle86
+ *
+ */
+@RestController
+@RequestMapping("/print")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello() {
+		
+		throw new WalleCoreException(ResponseType.SUCCESS);
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public JsonResponse<String> printTest(User user){
+		System.out.println("id="+user.getid_w());
+		System.out.println("name="+user.getname_w());
+		System.out.println(user.toString());
+		return new JsonResponse<>(user.toString());
+	}
+}
---
+++ src/com/walle/le/vo/User.java
@@ -0,0 +1,33 @@
+package com.walle.le.vo;
+
+/**
+ * 
+ * @author walle86
+ *
+ */
+
+public class User {
+	
+	private int id_w = 0;
+	private String name_w = "";
+
+	public void setid_w(int id) {
+		this.id_w = id;
+	}
+
+	public int getid_w() {
+		return this.id_w;
+	}
+
+	public void setname_w(String name) {
+		this.name_w = name;
+	}
+
+	public String getname_w() {
+		return this.name_w;
+	}
+	@Override
+	public String toString(){
+		return "id_w = "+id_w+",name_w = "+name_w;
+	}
+}
